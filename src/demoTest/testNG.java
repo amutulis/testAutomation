4fa12ad6e2f23683a99c168229d2ed00e24db35c
artismutulis <artismutulis@gmail.com>
2019-06-17 19:42:31 +0300

    Changed Selenium library from 3.0.1 to 3.11.0. Added Firefox browser.

--- src/demoTest/testNG.java
+++ src/demoTest/testNG.java
@@ -1,7 +1,5 @@
 package demoTest;
 
-
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -11,26 +9,28 @@ import org.junit.Assert;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 import pages.HomePage;
 import pages.ResultPage;
 
-
 public class testNG {
-	
 
 	public WebDriver driver;
-	
-	
+
 	String title;
 	String titleExpected = "Euroffice Discount Office Supplies and Office Stationery";
 	String resultTitleExpected = "Search";
 	public String baseUrl = "https://www.euroffice.co.uk/";
-	String driverPath = "C:\\Users\\Artis Mutulis\\Desktop\\chromedriver_win32\\chromedriver.exe";
+	// String driverPath = "C:\\Users\\Artis
+	// Mutulis\\Desktop\\chromedriver_win32\\chromedriver.exe";
+	String driverPath = "C:\\Users\\Artis Mutulis\\Desktop\\geckodriver.exe";
 	String searchPhrase = "Dymo Labelwriter";
+
 	public static boolean isSorted(double[] s) {
-		for (int i = 0; i < s.length-1; i++) {
-			if (s[i] < s[i + 1]) { 
+		for (int i = 0; i < s.length - 1; i++) {
+			if (s[i] < s[i + 1]) {
 				return true;
 			}
 		}
@@ -38,41 +38,43 @@ public class testNG {
 	}
 
 	@BeforeTest
-	//Open browser and goes to specified webpage
+	// Open browser and goes to specified webpage
 	public void prepare() throws InterruptedException {
-	System.setProperty("webdriver.chrome.driver", driverPath);
-		driver = new ChromeDriver();
+		/*
+		 * System.setProperty("webdriver.chrome.driver", driverPath); driver = new
+		 * ChromeDriver();
+		 */
+		System.setProperty("webdriver.gecko.driver", driverPath);
+		driver = new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
 		driver.get(baseUrl);
-		
-		
 
 	}
 
-	@Test(priority=0)
+	@Test(priority = 0)
 	public void checkSortedElements() throws InterruptedException {
 		HomePage homeP = new HomePage(driver);
-		ResultPage resultP= new ResultPage(driver);
+		ResultPage resultP = new ResultPage(driver);
 		Thread.sleep(1000);
 		String title = homeP.getPageTitle();
-		//Checking if test is on correct page
-		Assert.assertTrue("Title is inccorect",title.equalsIgnoreCase(titleExpected));
+		// Checking if test is on correct page
+		Assert.assertTrue("Title is inccorect", title.equalsIgnoreCase(titleExpected));
 		homeP.search(searchPhrase);
-		ArrayList<String> saraksts = new ArrayList<String>();	
-		//resultP = new ResultPage(driver);
+		ArrayList<String> saraksts = new ArrayList<String>();
+		// resultP = new ResultPage(driver);
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		String resultTitle = resultP.getPageTitle();
-		//Checking if test is on correct page
-		Assert.assertTrue("Title is inccorect",resultTitle.equalsIgnoreCase(resultTitleExpected));
+		// Checking if test is on correct page
+		Assert.assertTrue("Title is inccorect", resultTitle.equalsIgnoreCase(resultTitleExpected));
 		List<WebElement> priceList;
 		List<WebElement> pages;
-		
+
 		resultP.selectSortValue("PriceAscending");
 		Thread.sleep(1500);
 		priceList = resultP.getPriceList();
 		pages = resultP.getPages();
-		//Go trough all pages and retrive prices for searched elements
+		// Go trough all pages and retrive prices for searched elements
 		for (int j = 0; j < pages.size(); j++) {
 			for (int i = 0; i < priceList.size(); i++) {
 				js.executeScript("window.scrollTo(0,document.body.scrollHeight)");
@@ -83,21 +85,18 @@ public class testNG {
 			resultP.clickNext();
 			priceList = resultP.getPriceList();
 		}
-		//Converts String ArrayList to double Array
+		// Converts String ArrayList to double Array
 		double[] newDouble = new double[saraksts.size()];
 		for (int z = 0; z < saraksts.size(); z++) {
 			newDouble[z] = Double.parseDouble(saraksts.get(z));
 		}
-		
-		//Validates if retrieved list was sorted
-		Assert.assertTrue("Element is not sorted",isSorted(newDouble));
-		//Prints list
-		System.out.println( Arrays.toString(newDouble));		
-		driver.close();
-	
-	}
-	
 
-	
+		// Validates if retrieved list was sorted
+		Assert.assertTrue("Element is not sorted", isSorted(newDouble));
+		// Prints list
+		System.out.println(Arrays.toString(newDouble));
+		driver.close();
+
+	}
 
 }
--- src/demoTest/testNG2.java
+++ src/demoTest/testNG2.java
@@ -1,6 +1,5 @@
 package demoTest;
 
-
 import static org.testng.Assert.assertTrue;
 
 import java.util.concurrent.TimeUnit;
@@ -8,6 +7,8 @@ import java.util.concurrent.TimeUnit;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import pages.HomePage;
@@ -23,34 +24,39 @@ public class testNG2 {
 	String resultTitleExpected = "Search";
 	public String baseUrl = "https://www.euroffice.co.uk/";
 	String driverPath = "C:\\Users\\Artis Mutulis\\Desktop\\chromedriver_win32\\chromedriver.exe";
+	// String driverPath = "C:\\Users\\Artis Mutulis\\Desktop\\geckodriver.exe";
 	String searchPhrase = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-	
+
 	@BeforeTest
 	public void prepare() {
 		System.setProperty("webdriver.chrome.driver", driverPath);
 		driver = new ChromeDriver();
+		/*
+		 * System.setProperty("webdriver.gecko.driver", driverPath); driver = new
+		 * FirefoxDriver();
+		 */
 		driver.manage().window().maximize();
 		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
 		driver.get(baseUrl);
-		
 
 	}
- 
-  @Test(priority=1)
-  //Test case that test if phrase was shortened to 200 symbols, because of max length.
+
+	@Test(priority = 1)
+	// Test case that test if phrase was shortened to 200 symbols, because of max
+	// length.
 	public void checkSearchMaxLenght() throws InterruptedException {
-	  homeP = new HomePage(driver);
+		homeP = new HomePage(driver);
 		String title = homeP.getPageTitle();
-		//Checking if test is on correct page
-		Assert.assertTrue("Title is inccorect",title.equalsIgnoreCase(titleExpected));		
+		// Checking if test is on correct page
+		Assert.assertTrue("Title is inccorect", title.equalsIgnoreCase(titleExpected));
 		homeP.search(searchPhrase);
-		resultP = new ResultPage(driver);			
+		resultP = new ResultPage(driver);
 		String resultTitle = resultP.getPageTitle();
-		//Checking if test is on correct page
-		Assert.assertTrue("Title is inccorect",resultTitle.equalsIgnoreCase(resultTitleExpected));
+		// Checking if test is on correct page
+		Assert.assertTrue("Title is inccorect", resultTitle.equalsIgnoreCase(resultTitleExpected));
 		phrase = resultP.getSearchBoxPhrase();
-		//Validates if phrase is shortened
-		assertTrue((phrase.length()==200),"Phrase is longer than 200 symbols"); 
+		// Validates if phrase is shortened
+		assertTrue((phrase.length() == 200), "Phrase is longer than 200 symbols");
 		Thread.sleep(500);
 		driver.close();
 	}
